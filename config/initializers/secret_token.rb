c0c581e95d5db30523f4bb2819045b2f3976e892
Keith Webber <keith.e.webber@gmail.com>
2014-04-04 13:34:32 -0400

    init commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SystemsCheck7::Application.config.secret_key_base = 'bd947c3aed07845d4034e14bce4f2b731da3f1dfdf7c23fb4ee5c89f3c9bace44e6aa4e9b93f6a0a486ae7249b778aaa2546de3043862eadb3bbb25402395d9e'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+SystemsCheck7::Application.config.session_store :cookie_store, key: '_systems_check_7_session'
