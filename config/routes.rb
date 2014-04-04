788fcd02904d9e113ee92174ad2db70233a814b7
Keith Webber <keith.e.webber@gmail.com>
2014-04-04 13:47:11 -0400

    installed: Foundation, SimpleForm, Devise, rspec

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 SystemsCheck7::Application.routes.draw do
+
+  root to: "static#welcome"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
